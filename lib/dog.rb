77d6bf5721d2ad772563cb3c52865434ca177d0c
github username <github email address>
2019-11-15 11:57:58 -0800

    doooooone~

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,29 @@
-# Add your code here
+class Dog
+
+    attr_accessor :name
+
+    @@all = []
+
+    def initialize(name)
+        @name = name
+        save
+    end 
+
+    def self.all  # returns all Dog instances
+        @@all
+    end 
+
+    def self.clear_all  # deletes all existing dog instances
+        @@all.clear
+    end 
+
+    def self.print_all  # <iterate> thru all ind dog instances & puts out
+        puts @@all.map { |dog| dog.name }       # their names 
+    end 
+
+    def save    #creates save methods. flip places with initialize
+        @@all << self 
+    end 
+
+
+end 
